0c700ea0bd687b892e97f394c95572a346f2f31c
Tejas L P <ee26btech11231@iith.ac.in>
2026-09-09 15:31:03 +0530

    u

--- 09-09-2026/p3.c
+++ 09-09-2026/p3.c
@@ -9,14 +9,15 @@ char temp=str[0];
 str[0]=str[1];
 str[1]=temp;
 }
+//Main program begins
 int main()
 {
 //declaring string before acepting from user
 char str[21];
 printf("Enter string:");
 //accepting string
-scanf("%20s[^\n]",str);
-//checking if string is atleast 2 characters 
+scanf("%20[^\n]",str);
+//checking if string is atleast 2 characters long
 if(str[1]=='\0')
 {
 printf("Input string must atleast be 2 characters long");
